191205b105559f992039ef83fc9a676cf643db6d
User <User@DESKTOP-AMQNG48>
2021-04-29 18:34:03 -0500

    Prueba 2 de Actividad 3

--- src/actividad3/ACTIVIDAD3.java
+++ src/actividad3/ACTIVIDAD3.java
@@ -27,31 +27,76 @@ public class ACTIVIDAD3 {
         encabezados();
         separador();
 
-        //1.- Imprimir la suma consecutiva del 0 al 102.
+        /*/1.- Imprimir la suma consecutiva del 0 al 102.
         System.out.println("[----------EJERCICIO 1-------------]");
        
         int num = 0;
         int suma = 0;
-       System.out.println("Ingrese un número:");
-        
-        while (num <= 102) {
+       
+        while (num <= 10) {
  
             System.out.println("EL NÚMERO ES:" + num);
             suma += num;
             num++;
 
         }
-        System.out.println("LA SUMA DE TODOS LOS NÚMRROS ES:" + suma);
+       System.out.println("LA SUMA DE TODOS LOS NÚMRROS ES:" + suma);
+        separador();
+        System.out.println(" ");*/
+
+ /*2.Calcular el factorial del 1 al 103.
+        System.out.println("[----------EJERCICIO 2-------------]");
+        
+        System.out.println("EL FACTORIAL ES : ");
+        int nuM = 1;
+        int fact = 1;
+        for (int i = 0; i <= 10; i++) {
+            fact = fact * nuM;
+            nuM++;
+            System.out.println(fact);
+        }*/
+ /* separador();
+        System.out.println(" ");
+
+        //3.Calcular el factorial de un número que sea introducido por el usuario
+        System.out.println("[----------EJERCICIO 3-------------]");
+        int numE;
+        int contador = 1;
+        int oper = 1;
+
+        System.out.println("Ingrese un numero: ");
+        numE = teclado.nextInt();
+
+        for (int i = 1; i < numE; i++) {
+            contador = contador + 1;
+            oper = oper * contador;
+
+        }
+        System.out.println("El factorial es: " + oper);*/
         separador();
         System.out.println(" ");
-        
-        //2.Calcular el factorial del 1 al 103.
-        System.out.println("[----------EJERCICIO 1-------------]");
-        
-        
-        
-        
 
+        //4.- Calcular la media de la suma del 0 hasta el número introducido por el usuario
+        System.out.println("[----------EJERCICIO 4-------------]");
+        
+        System.out.println("INGRESE UN NÚMERO PARA ENCONTRAR SU MEDIA:");
+        
+        int suMa = 0;
+        int nuMe = 0;
+        int cont = teclado.nextInt();
+        for (int i = cont; i >=0 ; i--) {
+            suMa= suMa + nuMe;
+            nuMe++;
+        }
+        float media = suMa/cont;
+        System.out.println("LA MEDIA ES:" + media);
+        
+        
+        
+        
+        
+        
+        
+        
     }
-
 }

6c6efedeb72bd022c03b2d494b0cdcd0cc48d53c
User <User@DESKTOP-AMQNG48>
2021-04-29 12:50:48 -0500

    Prueba 1 de Actividad 3

--- src/actividad3/ACTIVIDAD3.java
+++ src/actividad3/ACTIVIDAD3.java
@@ -1,5 +1,7 @@
 package actividad3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ÁNGEL_HIDALGO
@@ -21,21 +23,34 @@ public class ACTIVIDAD3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
         encabezados();
         separador();
 
         //1.- Imprimir la suma consecutiva del 0 al 102.
         System.out.println("[----------EJERCICIO 1-------------]");
-
+       
         int num = 0;
         int suma = 0;
-
+       System.out.println("Ingrese un número:");
+        
         while (num <= 102) {
+ 
             System.out.println("EL NÚMERO ES:" + num);
             suma += num;
             num++;
+
         }
         System.out.println("LA SUMA DE TODOS LOS NÚMRROS ES:" + suma);
+        separador();
+        System.out.println(" ");
+        
+        //2.Calcular el factorial del 1 al 103.
+        System.out.println("[----------EJERCICIO 1-------------]");
+        
+        
+        
+        
 
     }
 
